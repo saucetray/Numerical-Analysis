362afec36dbd42ebba6b947b4a8a8bc6f38e62a2
saucetray <justintimetotheparty@gmail.com>
2020-04-16 22:05:53 -0400

    BackwardEuler is working and Graph Instructions

--- ODE/ODESolvers.hs
+++ ODE/ODESolvers.hs
@@ -4,43 +4,52 @@
 -- Can solve anything from dx/dt = f(x(t), t)
 -- Dependencies: Chart and Chart-Cairo
 
-module ForwardEuler where
-
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
+
 -- Using two-point formula to approximate the derivative at this point
-derivative :: (Float -> Float -> Float) -> Float -> Float -> Float -> Float
-derivative f x t h = (y2 - y1) / (2 * h)
-                     where y2 = f (x+h) t
-                           y1 = f (x-h) t
+f' :: (Float -> Float -> Float) -> Float -> Float -> Float -> Float
+f' f x t h = (y2 - y1) / (2 * h)
+             where y2 = f (x+h) t
+                   y1 = f (x-h) t
 
--- Newton's method for functions of 1 variable but relies on a constant input
--- because of the nature of dx/dt = f(x(t), t)
-newton :: (Float -> Float -> Float) -> Float -> Float -> Float -> Float -> Float
-newton f x t tol h = let eval = f x t in
+
+-- newton's method for backwardEuler
+newtonbe :: (Float -> Float -> Float) -> Float -> Float -> Float -> Float -> Float -> Float
+newtonbe f x t g tol h = let eval = abs $ g - x - h * f g t in
                        if eval < tol
-                          then x
-                               else newton f x0 t tol h
-                                    where x0 = x - f x t / derivative f x t h
+                          then g
+                             else newtonbe f x t g0 tol h
+                                where g0 = g - (g - x - h * f g t) / (1 - h * f' f g t h)
 
 
+-- forwardEuler that is a good choice for simple ODEs
 forwardEuler :: (Float -> Float -> Float) -> Float -> Float ->
                  Float -> Int -> [(Float, Float)]
-forwardEuler func h t x s = if s > 0
-                            then (t, x) : forwardEuler func h nextT nextX nextS
-                            else []
-                            where nextT = t + h
-                                  nextX = x + h * func x t
-                                  nextS = s - 1
+forwardEuler f h x t s = if s > 0
+                            then (t, x) : forwardEuler f h x0 t0 s0
+                               else []
+                                  where t0 = t + h
+                                        x0 = x + h * f x t
+                                        s0 = s - 1
 
 
 -- backwardEuler that is much more stable for solving dx/dt = f(x(t), t)
 backwardEuler :: (Float -> Float -> Float) -> Float -> Float ->
                   Float -> Int -> [(Float, Float)]
-backwardEuler func h t x s = if s > 0
-                             then (t, x) : backwardEuler func h nextT nextX nextS
-                             else []
-                             where nextT = t+h
-                                   nextX = x + h * func x t
-                                   nextS = s - 1
+backwardEuler f h x t s =  if s > 0
+                              then (t, x) : backwardEuler f h x0 t0 s0
+                                 else []
+                                    where t0 = t + h
+                                          x0 = x + h * f (newtonbe f x t 4 h h) t
+                                          s0 = s - 1
+
+
+-- Make a graph!
+backwardEulerGraph :: (Float -> Float -> Float) -> Float
+                      -> Float -> Float -> Int -> String
+                      -> String -> IO()
+backwardEulerGraph f x t h s filename funcname = toFile def filename $ do
+    layout_title .= "Solution to dx/dt = " ++ funcname
+    plot (line "F(x)" [backwardEuler f h x t s])
